cdc417c48ac532f3ab6d3a34d90be34b753e4cb7
Andrzej Fricze <andrzej.fricze@functionite.com>
2018-06-24 12:15:43 +0200

    Let's layout'em

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import './layout/reset.css'
+import './layout/fonts.css'
+
+import { ChatInput } from './blocks/ChatInput.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Halko w czate</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ChatInput></ChatInput>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/blocks/ChatInput.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const ChatInput = ({ }) => <input type="text" />
